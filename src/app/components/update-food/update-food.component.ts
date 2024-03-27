b7fbf22d482f961a5d558d6fec84f546df36d575
cronus712 <rayengrid@gmail.com>
2024-03-27 16:38:24 +0100

    update food item ts

--- src/app/components/update-food/update-food.component.ts
+++ src/app/components/update-food/update-food.component.ts
@@ -16,8 +16,14 @@ export class UpdateFoodComponent {
   starss = [1, 2, 3, 4, 5];
   faStar = faStar;
   faHeart = faHeart;
-  // rating : number = 0;
+  rating : number = 0;
   // name = new FormControl('');
+   name !: string;
+   description !: string;
+   image !: string ;
+   imageFile !: File | null;
+   imagePreview: string | null = null;
+   favorite : boolean = false;
 
   constructor(private foodService : FoodService, private route : ActivatedRoute, private fb : FormBuilder) {}
 
@@ -38,6 +44,55 @@ export class UpdateFoodComponent {
 
   toggleFavorite() {
     this.food.favorite = !this.food.favorite;
+  }
+
+  onImageSelected(event: any) {
+    // this.imageFile = event.target.files[0];
+    if (event.target.files && event.target.files[0]) {
+      this.imageFile = event.target.files[0]; // Store the selected file
+      const reader = new FileReader();
+      reader.readAsDataURL(this.imageFile! as Blob);  // Read the file
+      reader.onload = () => {
+        this.imagePreview = reader.result?.toString() || '';
+      };
+    } else {
+      console.log('here')
+      this.imageFile = null; // Clear imageFile and imagePreview
+      this.imagePreview = null;
+    }
+  }
+
+  save() {
+    if ( this.imageFile) {
+      const reader = new FileReader();
+      reader.readAsDataURL(this.imageFile); // Read the image file
+
+      reader.onload = () => {
+        // On successful reading
+        this.image = reader.result?.toString() || ''; // Extract base64 string
+        // const newFoodItem: Food = {
+        //   name: this.name,
+        //   description: this.description,
+        //   image: this.image, // Use the base64 encoded image
+        //   rating: this.rating,
+        //   favorite: this.favorite
+        // };
+
+        this.food.name = this.name;
+        this.food.description = this.description;
+        this.food.image = this.image;
+        this.food.rating = this.rating;
+        this.food.favorite = this.favorite;
+
+      };
+
+      reader.onerror = (error) => {
+        console.error("Error reading image file:", error);
+      };
+    }
+    this.foodService.updateFood(this.food).subscribe();
+
+      console.log('here')
   }
 
 }
